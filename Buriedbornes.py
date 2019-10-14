725afd79733f09eeea5ce5f0bef8c9b20709b6b9
sewon-yun(Notebook) <55976914+sewon-yun@users.noreply.github.com>
2019-10-14 23:59:36 +0900

    2019_10_14 클래스

--- Buriedbornes.py
+++ Buriedbornes.py
@@ -2,6 +2,27 @@ from pico2d import *
 
 BG_WIDTH, BG_HEIGHT = 600, 800
 
+class Character:
+    def __init__(self):
+        self.image_darkelf = load_image('darkelf.png')
+        self.image_fairy = load_image('fairy.png')
+        self.image_duelist = load_image('duelist.png')
+        self.image_grave_robber = load_image('grave robber.png')
+        self.image_vampire = load_image('vampire.png')
+        self.image_witch = load_image('witch.png')
+        self.x, self.y = 200, 200
+    def draw(self):
+        self.image_darkelf.clip_draw(0, 0, 800, 800, self.x, self.y, 300, 300)
+    def update(self):
+        pass
+
+class Monster:
+    def __init__(self):
+        self.image_rabbit = load_image('rabbit.png')
+        self.x, self.y = 425, 600
+    def draw(self):
+        self.image_rabbit.clip_draw(0, 0, 800, 800, self.x, self.y, 300, 300)
+
 def handle_events():
     global running
     global x, y
@@ -22,8 +43,6 @@ def handle_events():
 open_canvas(BG_WIDTH, BG_HEIGHT)
 
 background = load_image('background.png')
-monster_rabbit = load_image('rabbit.png')
-character_darkelf = load_image('darkelf.png')
 cursor = load_image('cursor.png')
 hp_box = load_image('hp_box.png')
 skill_9 = load_image('skill_9.png')
@@ -33,14 +52,24 @@ running = True
 
 turn = 0
 
+character = Character()
+monsters = [Monster() in range(1)]
+
 x, y = BG_WIDTH // 2, BG_HEIGHT // 2
 
 while running:
+    handle_events()
+
+
     clear_canvas()
     background.draw(BG_WIDTH // 2, BG_HEIGHT // 2)
-    character_darkelf.clip_draw(0, 0, 800, 800, 200, 200, 300, 300)
+
+    character.draw()
+
+    # for monster in monsters:
+    #     monster.draw()
+
     background.clip_draw(0, 0, 600, 200, 0, 120, 1200, 105)
-    monster_rabbit.clip_draw(0, 0, 800, 800, 425, 600, 300, 300)
     hp_box.clip_draw(0, 0, 200, 100, 450, 250, 250, 125)
     hp_box.clip_draw(0, 0, 200, 100, 150, 650, 250, 125)
     skill_9.clip_draw(200, 300, 60, 50, 60, 120, 110, 100)
@@ -54,7 +83,6 @@ while running:
     cursor.clip_draw(0, 0, 39, 37, x + 10, y - 10, 30, 30)
     update_canvas()
 
-    handle_events()
     delay(0.01)
 
 close_canvas()
